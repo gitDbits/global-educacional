49cb726b11114e37be2095793c3fab4ca4c8bcb4
Diego Noronha <diego.noronha@bemobi.com>
2024-03-05 11:24:26 -0400

    feat: add filter city

--- app/controllers/portal/home_controller.rb
+++ app/controllers/portal/home_controller.rb
@@ -6,6 +6,7 @@ module Portal
     layout 'admin'
 
     def home
+      @cities = User.where(admin: nil).distinct.pluck(:city).compact
       @q = User.where(admin: nil).order(id: :desc).ransack(params[:q])
       @pagy, @users = pagy(@q.result(distinct: true))
     end
